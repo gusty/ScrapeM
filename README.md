d848fdcac716be1838f418f14e53617a530121e5
Gustavo Leon <gustavo.perezleon@gmail.com>
2016-12-07 22:47:52 +0100

    Proof of concept

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ScrapeM
 A monadic web scraping library
+
+Scrapes the web with category
+
+This is essentially a single-function library which in fact integrates many existing libraries and present several ways to approach web scraping by using different monads.
+
+It's possible to create stateful queries which simulates basic user interaction with form submission, cookie handling and at the same time maintain multiple independent navigations paths driven by the data.
---
+++ Sample-StateT-Seq-1.fsx
@@ -0,0 +1,29 @@
+﻿#load "ScrapeM.fsx"
+open System
+open System.Text.RegularExpressions
+open System.Collections.Generic
+open FSharp.Data
+open FSharpPlus
+open ScrapeM
+
+let request a b: StateT<_,_ seq> = hoistState (ScrapeM.request a b) // Use stateful sequences
+
+type News = {User : string; Title : string; IntroText : string}
+
+let q = linq {
+    for credential in lift [("stopthebug", "stopthebug1"); ("aslzzztk", "abcd1234")] do
+    for data       in lift [[KeyValuePair ("username", fst credential); KeyValuePair ("password", snd credential)]] do
+    let!  _  = request "https://secure.moddb.com/members/login" None                        
+    let!  _  = request "" (Some data)                                                       
+    let! htm = request ("http://www.moddb.com/members/" + fst credential + "/comments") None
+    for lnk in htm |> parse |> cssSelect "a[class='related'] a[href^='/news']" |>> (fun x -> x.AttributeValue("href") , x.InnerText()) |> distinct |> lift do
+    let! doc = (request ("http://www.moddb.com" + fst lnk) None) |>> parse
+    select {
+        User      = fst credential
+        Title     = snd lnk
+        IntroText = (cssSelect "p[class='introductiontext']" doc).Head.InnerText() } into news
+    where (length news.IntroText < 150)
+    select news}
+
+let news = StateT.run q Context.Empty |> Seq.map fst
+printTable news
